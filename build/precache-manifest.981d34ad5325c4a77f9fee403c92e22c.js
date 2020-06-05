a4fdd3f5246045ce07a354588e6c411d766b0628
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-06-05 16:42:32 +0300

    new Build

---
+++ build/precache-manifest.981d34ad5325c4a77f9fee403c92e22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd99bf162f72a6b531642c5045cdc720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0461ac9b2e527d2cd937",
+    "url": "/static/css/main.2fed08d3.chunk.css"
+  },
+  {
+    "revision": "2a3c044e4d60cc238502",
+    "url": "/static/js/2.4cc82441.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cc82441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0461ac9b2e527d2cd937",
+    "url": "/static/js/main.36c1faba.chunk.js"
+  },
+  {
+    "revision": "d6880fb4e91dc667f3ba",
+    "url": "/static/js/runtime-main.29e28a32.js"
+  }
+]);
